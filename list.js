2511f799cf0b74a343f506c79bd994286763b9c9
ramibr01 <ramibr01@luther.edu>
2015-10-19 14:28:44 -0500

    Update list.js

--- list.js
+++ list.js
@@ -1,6 +1,8 @@
 create = function() {
  newLi = document.createElement("li");
   newLi.className = myPriority.value;
+  newLi.appendChild(li);
  newIn = document.createElement("input");
   newIn.type = "checkbox";
+  newIn.appendChild(input);
  }
