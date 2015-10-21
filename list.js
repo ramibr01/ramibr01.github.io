9d622345159f5a59b3d83fadbf8c6ab3411f87f4
ramibr01 <ramibr01@luther.edu>
2015-10-21 15:21:15 -0700

    Update list.js

--- list.js
+++ list.js
@@ -1,11 +1,11 @@
 createNewTask = function() {
  myLi = document.createElement("li");
-   myLi.document.querySelector("#priority");
-   myLi.classlist.add(myPriority.value);
+  myLi.document.querySelector("#priority");
+  myLi.classlist.add(myPriority.value);
  myIn = document.createElement("input");
   myIn.type = "checkbox";
  myText = document.createTextNode(myBox.value);
-  myText.document.querySelector("#a");
+  myText.document.querySelector("#textbox");
  myUl.appendChild(myLi);
  myLi.appendChild(myIn);
  myLi.appendChild(myText);
